0cbcfe1d1f8f33c84b5aa17d637e5340f89330c1
Jimmy <jimmylanbcn@gmail.com>
2021-10-24 16:54:59 -0400

    Append default prefix to router dom wrapper errors

--- src/errors/RouterDomWrapperError.ts
+++ src/errors/RouterDomWrapperError.ts
@@ -4,12 +4,12 @@ export class RouterDomWrapperError extends Error implements Serializable {
   public type: string | undefined;
 
   constructor(message: string, public cause?: string) {
-    super(message);
+    super(`[Router DOM Wrapper] ${message}`);
     Object.setPrototypeOf(this, RouterDomWrapperError.prototype);
   }
 
   serialize(): string {
-    const reprTokens = [`[Router DOM Wrapper] ${this.message}`];
+    const reprTokens = [this.message];
     if (this.type) {
       reprTokens.push(`Error type: ${this.type}.`);
     }
