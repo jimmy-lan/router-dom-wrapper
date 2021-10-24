051a2ab92d9c488701ced19f820d296cc89ab257
Jimmy <jimmylanbcn@gmail.com>
2021-10-24 19:29:54 -0400

    Optimize imports

--- src/contexts/permissions/createPermissionsProvider.tsx
+++ src/contexts/permissions/createPermissionsProvider.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, PropsWithChildren } from "react";
+import React, { FunctionComponent } from "react";
 import {
   PermissionsProvider,
   PermissionsProviderProps,
