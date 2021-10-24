f484ab688b72cf087c5340af1d86e5cd7db2f514
Jimmy <jimmylanbcn@gmail.com>
2021-10-24 11:01:06 -0400

    Update docstring

--- src/contexts/routePermission/RoutePermissionProvider.tsx
+++ src/contexts/routePermission/RoutePermissionProvider.tsx
@@ -3,11 +3,11 @@ import { PermissionContext } from "./PermissionContext";
 import { IsAccessAllowedFunc, IsAuthenticatedFunc } from "../../types";
 
 export interface RoutePermissionProviderProps<PermissionIndicator = unknown> {
-  /**
-   * A function to check whether the user is authenticated.
-   * Return a boolean value indicating whether the user is authenticated.
-   */
+  /** A function to check whether the user is authenticated.
+   * Return a boolean value indicating whether the user is authenticated. */
   isAuthenticated: IsAuthenticatedFunc;
+  /** A function to check if the current logged in user has permission to
+   * access a resource with `permissionIndicator`. */
   isAccessAllowed: IsAccessAllowedFunc<PermissionIndicator>;
 }
 
