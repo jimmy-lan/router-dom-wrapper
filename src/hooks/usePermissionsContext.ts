13f7a79c0af623704eda4fa83bd0be7660f824b1
Jimmy <jimmylanbcn@gmail.com>
2021-10-24 16:10:12 -0400

    Throw InvalidContextError

--- src/hooks/usePermissionsContext.ts
+++ src/hooks/usePermissionsContext.ts
@@ -1,10 +1,14 @@
 import { useContext } from "react";
 import { PermissionsContextValues, PermissionsContext } from "../contexts";
+import { InvalidContextError } from "../errors/InvalidContextError";
 
 export const usePermissionsContext = (): PermissionsContextValues => {
   const contextValues = useContext(PermissionsContext);
   if (!contextValues) {
-    throw new Error();
+    throw new InvalidContextError(
+      "Could not use permissions context. " +
+        "Did you forget to include a PermissionsProvider?"
+    );
   }
   // We enforce the validity of context values by using the
   // props in `PermissionsProvider`.
