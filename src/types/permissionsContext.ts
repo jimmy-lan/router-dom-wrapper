80bdf2902d4faba587225d6e70c4ef04e54c72f7
Jimmy <jimmylanbcn@gmail.com>
2021-10-24 15:37:12 -0400

    Add redirect settings

--- src/types/permissionsContext.ts
+++ src/types/permissionsContext.ts
@@ -5,10 +5,22 @@ export type IsAccessAllowedFunc<Permission = unknown> = (
 ) => boolean;
 
 export interface PermissionsContextConfig {
+  redirect?: Partial<{
+    /** Path to redirect the users when they attempt to access a protected
+     * route without authenticating. */
+    unauthorized: string;
+    /** Path to redirect authenticated users when they attempt to access a
+     * resource without proper permission.<br/> **NOTE**: This setting will
+     * only be effective when `shouldRenderForbidden` is set to `true`. */
+    forbidden: string;
+  }>;
   /** A function to check whether the user is authenticated.
    * Return a boolean value indicating whether the user is authenticated. */
   isAuthenticated: IsAuthenticatedFunc;
   /** A function to check if the current logged in user has permission to
    * access a resource that requires `permission`. */
   isAccessAllowed: IsAccessAllowedFunc;
+  /** When set to `true`, routes that the users do not have permission to
+   * access will still be rendered on the DOM tree. Defaults to `false`. */
+  shouldRenderForbidden: boolean;
 }
