1690e96507166c785be77260777f4fd413eb4397
Jimmy <jimmylanbcn@gmail.com>
2021-10-24 11:05:48 -0400

    Migrate RoutePermissionProviderProps interface

--- src/types/RoutePermissionProvider.ts
+++ src/types/RoutePermissionProvider.ts
@@ -3,3 +3,12 @@ export type IsAuthenticatedFunc = () => boolean;
 export type IsAccessAllowedFunc<PermissionIndicator = unknown> = (
   permissionIndicator: PermissionIndicator
 ) => boolean;
+
+export interface RoutePermissionProviderProps<PermissionIndicator = unknown> {
+  /** A function to check whether the user is authenticated.
+   * Return a boolean value indicating whether the user is authenticated. */
+  isAuthenticated: IsAuthenticatedFunc;
+  /** A function to check if the current logged in user has permission to
+   * access a resource with `permissionIndicator`. */
+  isAccessAllowed: IsAccessAllowedFunc<PermissionIndicator>;
+}
