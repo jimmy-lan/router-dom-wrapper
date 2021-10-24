270da94e92c73486a2258e79f9021fe6888204f0
Jimmy <jimmylanbcn@gmail.com>
2021-10-24 15:21:05 -0400

    Index files and interfaces

--- src/contexts/routePermission/PermissionsContext.tsx
+++ src/contexts/routePermission/PermissionsContext.tsx
@@ -1,7 +1,7 @@
 import React, { createContext, PropsWithChildren } from "react";
 import { PermissionsContextConfig } from "../../types";
 
-interface PermissionContextValues extends PermissionsContextConfig {}
+export interface PermissionContextValues extends PermissionsContextConfig {}
 
 export const PermissionsContext = createContext<
   Partial<PermissionContextValues>
--- src/contexts/routePermission/PermissionsProvider.tsx
+++ src/contexts/routePermission/PermissionsProvider.tsx
@@ -2,9 +2,11 @@ import React, { FunctionComponent } from "react";
 import { PermissionsContext } from "./PermissionsContext";
 import { PermissionsContextConfig } from "../../types";
 
-interface Props extends PermissionsContextConfig {}
+export interface PermissionsProviderProps extends PermissionsContextConfig {}
 
-const PermissionsProvider: FunctionComponent<Props> = (props) => {
+const PermissionsProvider: FunctionComponent<PermissionsProviderProps> = (
+  props
+) => {
   const { children, ...otherValues } = props;
   return (
     <PermissionsContext.Provider value={otherValues}>
