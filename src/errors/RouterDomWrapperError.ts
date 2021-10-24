f6b4ae4f7f015c244f56d036cd0635b454763785
Jimmy <jimmylanbcn@gmail.com>
2021-10-24 16:05:52 -0400

    Add error type

--- src/errors/RouterDomWrapperError.ts
+++ src/errors/RouterDomWrapperError.ts
@@ -1,6 +1,8 @@
 import { Serializable } from "../types";
 
 export class RouterDomWrapperError extends Error implements Serializable {
+  public type: string | undefined;
+
   constructor(message?: string, public cause?: string) {
     super(message);
     Object.setPrototypeOf(this, RouterDomWrapperError.prototype);
@@ -8,6 +10,9 @@ export class RouterDomWrapperError extends Error implements Serializable {
 
   serialize(): string {
     const reprTokens = [`[Router DOM Wrapper] ${this.message}`];
+    if (this.type) {
+      reprTokens.push(`Error type: ${this.type}.`);
+    }
     if (this.cause) {
       reprTokens.push(`Caused by: ${this.cause}.`);
     }
