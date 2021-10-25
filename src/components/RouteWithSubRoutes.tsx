fb4e5108db3a215edf4864d3605699206944c16f
Jimmy <jimmylanbcn@gmail.com>
2021-10-24 20:26:55 -0400

    Rename RouteWithSubRoutes.tsx

---
+++ src/components/RouteWithSubRoutes.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from "react";
+
+interface Props {
+}
+
+const RouteWithSubRoutes: FunctionComponent<Props> = (props) => {
+
+  return ();
+};
+
+export { RouteWithSubRoutes };
