473b04c35c2039390d67d2f6bc7d0015c27889b4
Jimmy <jimmylanbcn@gmail.com>
2021-10-26 18:56:03 -0400

    Add docstring for new fields

--- src/types/permissionsContext.ts
+++ src/types/permissionsContext.ts
@@ -7,7 +7,21 @@ export type CheckAccessRightFunc<Permissions = unknown> = (
 ) => boolean;
 
 export interface PermissionsContextConfig {
+  /**
+   * When users attempt to access a protected route without authenticating,
+   * this field will be used to handle the situation.
+   * @see RedirectOrComponent
+   */
   unauthorizedHandle: RedirectOrComponent;
+  /**
+   * When users attempt to access a resource without property permission,
+   * this field will be used to handle the situation.
+   *
+   * **NOTE:** For routes rendered by the `<ConfiguredRoutes />` component,
+   * this setting will only be effective when `shouldRenderForbidden`
+   * is set to `true`.
+   * @see RedirectOrComponent
+   */
   forbiddenHandle: RedirectOrComponent;
   redirect?: Partial<{
     /** Path to redirect the users when they attempt to access a protected
