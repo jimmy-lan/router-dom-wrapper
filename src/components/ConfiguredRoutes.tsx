658ed2c55c2b04998cf75bb2cc839f1119a32017
Jimmy <jimmylanbcn@gmail.com>
2021-10-25 23:56:32 -0400

    Add ConfiguredRoutes structure

---
+++ src/components/ConfiguredRoutes.tsx
@@ -0,0 +1,9 @@
+import React, { FunctionComponent } from "react";
+
+interface Props {}
+
+const ConfiguredRoutes: FunctionComponent<Props> = (props) => {
+  return <></>;
+};
+
+export { ConfiguredRoutes };
