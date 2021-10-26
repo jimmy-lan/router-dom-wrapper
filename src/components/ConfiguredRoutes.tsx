9a1c29ef12f22346fffb60652a98129ffbae788c
Jimmy <jimmylanbcn@gmail.com>
2021-10-26 06:06:27 -0400

    Format docstring

--- src/components/ConfiguredRoutes.tsx
+++ src/components/ConfiguredRoutes.tsx
@@ -1,6 +1,19 @@
 import React, { FunctionComponent } from "react";
+import { RouteEntry } from "../types";
 
-interface Props {}
+interface Props {
+  /** Configured route entries array to use. */
+  entries: RouteEntry[];
+  /**
+   * When set to true, routes will be rendered _inclusively_.
+   * That is, all routes configured in `props.entries` matching the current
+   * URL will be rendered. If set to false, the first matching route in
+   * `props.entries` array will be rendered _exclusively_.
+   * Defaults to `false`.
+   * @see https://reactrouter.com/web/api/Switch
+   */
+  disableSwitch?: boolean;
+}
 
 const ConfiguredRoutes: FunctionComponent<Props> = (props) => {
   return <></>;
