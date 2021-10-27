f0b594822003e06ad9b4783c62ef768289408020
Jimmy <jimmylanbcn@gmail.com>
2021-10-26 21:35:01 -0400

    Add handleRedirectOrComponent.tsx

---
+++ src/utils/handleRedirectOrComponent.tsx
@@ -0,0 +1,26 @@
+import React, { cloneElement, FunctionComponent } from "react";
+import { Redirect, useLocation } from "react-router-dom";
+import { RedirectOrComponent } from "../types";
+
+const RedirectWrapper: FunctionComponent<{ pathname: string }> = (props) => {
+  const { pathname } = props;
+  const location = useLocation();
+  return <Redirect to={{ pathname, state: { from: location } }} />;
+};
+
+export const handleRedirectOrComponent = (
+  field: RedirectOrComponent,
+  additionalProps: Record<string, any>
+) => {
+  if (typeof field === "string") {
+    return <RedirectWrapper pathname={field} />;
+  }
+  if ("type" in field) {
+    if (additionalProps) {
+      return cloneElement(field, additionalProps);
+    }
+    return field;
+  }
+  const Component = field;
+  return <Component {...additionalProps} />;
+};
