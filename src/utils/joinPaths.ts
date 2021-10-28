0ef879ef4302955838a994bc5450189b5ca6147c
Jimmy <jimmylanbcn@gmail.com>
2021-10-28 18:08:16 -0400

    Handle edge case

--- src/utils/joinPaths.ts
+++ src/utils/joinPaths.ts
@@ -1,4 +1,8 @@
 export const joinPaths = (path1: string, path2: string) => {
+  // Exclude root level "*" path matcher.
+  if (path1 === "/" && path2 === "*") {
+    return path2;
+  }
   const processedPath1 = path1.endsWith("/")
     ? path1.slice(0, path1.length - 1)
     : path1;
