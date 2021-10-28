75a933cf94f0477e4347fe0f0043a931d72a1783
Jimmy <jimmylanbcn@gmail.com>
2021-10-28 18:22:11 -0400

    Add RouterWrapperProps.ts

---
+++ src/types/RouterWrapperProps.ts
@@ -0,0 +1,5 @@
+import { RouterProps } from "react-router-dom";
+
+export interface RouterWrapperProps extends RouterProps {
+  path: string;
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,4 @@ export * from "./permissionsContext";
 export * from "./Serializable";
 export * from "./RouteEntry";
 export * from "./RedirectOrComponent";
+export * from "./RouterWrapperProps";
