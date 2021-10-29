2237ac91ddc503d4e32b58b12c0739cf1e65bea3
Jimmy <jimmylanbcn@gmail.com>
2021-10-29 19:12:09 -0400

    Adjust docstring

--- src/utils/handleRedirectOrComponentField.tsx
+++ src/utils/handleRedirectOrComponentField.tsx
@@ -2,6 +2,7 @@ import React, { cloneElement, FunctionComponent } from "react";
 import { Redirect, useLocation, useRouteMatch } from "react-router-dom";
 import { RedirectOrComponent } from "../types";
 import { RedirectError } from "../errors";
+import { handleComponentField } from "./handleComponentField";
 
 const RedirectWrapper: FunctionComponent<{ pathname: string }> = (props) => {
   const { pathname } = props;
@@ -21,8 +22,8 @@ const RedirectWrapper: FunctionComponent<{ pathname: string }> = (props) => {
  * Given a redirect-or-component handle, return the appropriate component
  * to render based on the input.
  * @param {RedirectOrComponent} field The redirect-or-component handle to parse.
- * @param {Record<string, any>} extraProps If `field` is a component, pass
- *   return the constructed component with these additional props.
+ * @param {Record<string, any>} extraProps If `field` is a component, return
+ * the constructed component with these additional props.
  * @see RedirectOrComponent
  */
 export const handleRedirectOrComponentField = (
@@ -32,4 +33,5 @@ export const handleRedirectOrComponentField = (
   if (typeof field === "string") {
     return <RedirectWrapper pathname={field} />;
   }
+  return handleComponentField(field, extraProps);
 };
