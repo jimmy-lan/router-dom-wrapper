8b41826896d61f65d7aba19e9728bec73455c437
Jimmy <jimmylanbcn@gmail.com>
2021-10-29 18:50:07 -0400

    Add renderRoutes docstring

--- src/functions/renderRoutes.ts
+++ src/functions/renderRoutes.ts
@@ -3,6 +3,11 @@ import { renderSingleRoute } from "./renderSingleRoute";
 import { ReactNode } from "react";
 import { getRouteKey } from "../utils";
 
+/**
+ * Return components corresponding to `routes` configuration.
+ * @param routes Configuration to parse.
+ * @param extraProps Extra props to pass into each route component.
+ */
 export const renderRoutes = (
   routes: RouteEntry[],
   extraProps?: Record<string, any>
