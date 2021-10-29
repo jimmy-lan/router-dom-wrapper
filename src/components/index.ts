087d8bf10ba5def700bf8d36fa2259274c168f98
Jimmy <jimmylanbcn@gmail.com>
2021-10-29 07:35:22 -0400

    Remove RouteWithSubRoutes.tsx

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,3 @@
 export * from "./ProtectedRoute";
 export * from "./PublicRoute";
-export * from "./RouteWithSubRoutes";
 export * from "./ConfiguredRoutes";
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from "./contexts";
 export * from "./errors";
 export * from "./hooks";
 export * from "./types";
+export * from "./functions";
