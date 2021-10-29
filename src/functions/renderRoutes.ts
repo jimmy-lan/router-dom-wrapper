8064eed1f6e698356bb34231cbee84aa53592b40
Jimmy <jimmylanbcn@gmail.com>
2021-10-29 18:48:38 -0400

    Use getRotueKey function

--- src/functions/renderRoutes.ts
+++ src/functions/renderRoutes.ts
@@ -1,12 +1,14 @@
 import { RouteEntry } from "../types";
 import { renderSingleRoute } from "./renderSingleRoute";
 import { ReactNode } from "react";
+import { getRouteKey } from "../utils";
 
 export const renderRoutes = (
   routes: RouteEntry[],
   extraProps?: Record<string, any>
 ): ReactNode[] => {
-  return routes.map((route: RouteEntry) =>
-    renderSingleRoute(route, { key: route.path, ...extraProps })
-  );
+  return routes.map((route: RouteEntry) => {
+    const key = getRouteKey(route);
+    return renderSingleRoute(route, { key, ...extraProps });
+  });
 };
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,3 @@
 export * from "./joinPaths";
+export * from "./getRouteKey";
 export * from "./handleRedirectOrComponentField";
