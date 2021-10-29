86bb9f90e5dba3cb05597a8ae35008af7120a8dd
Jimmy <jimmylanbcn@gmail.com>
2021-10-29 19:12:03 -0400

    Add docstring

--- src/utils/handleComponentField.tsx
+++ src/utils/handleComponentField.tsx
@@ -1,6 +1,15 @@
 import { Component } from "../types";
 import React, { cloneElement } from "react";
 
+/**
+ * Given a component field config, return an appropriate constructed component
+ * to render. For more information on component fields, please see its
+ * type definition.
+ * @param {Component} field The component field to render.
+ * @param {Record<string, any>} extraProps Return the constructed component
+ *   with these props.
+ * @see Component
+ */
 export const handleComponentField = (
   field: Component,
   extraProps?: Record<string, any>
