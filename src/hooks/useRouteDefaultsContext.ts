9605cb305d414b3a71bb4df2f9d56e79bd67d650
Jimmy <jimmylanbcn@gmail.com>
2021-10-29 21:22:50 -0400

    Allow disableProviderCheck setting

--- src/hooks/useRouteDefaultsContext.ts
+++ src/hooks/useRouteDefaultsContext.ts
@@ -2,9 +2,21 @@ import { useContext } from "react";
 import { RouteDefaultsContext } from "../contexts";
 import { InvalidContextError } from "../errors";
 
-export const useRouteDefaultsContext = () => {
+interface Options {
+  /** When set to `true`, do not check for whether a valid provider is
+   * included. Return `undefined` if no provider is present. Defaults
+   * to `false`. You can set this value to `true` when you only want to
+   * reference route default values, and can handle the case when the return
+   * value from this call is `undefined`. */
+  disableProviderCheck?: boolean;
+}
+
+export const useRouteDefaultsContext = (options?: Options) => {
   const contextValues = useContext(RouteDefaultsContext);
-  if (!contextValues || !contextValues.setRouteDefaults) {
+  if (
+    !options?.disableProviderCheck &&
+    (!contextValues || !contextValues.setRouteDefaults)
+  ) {
     throw new InvalidContextError(
       "Could not use route defaults context. " +
         "Did you forget to include a RouteDefaultsProvider?"
